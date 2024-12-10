a68c2235649e608e018a3c5564fd4247097e0737
caicai0402 <caicaihu0402@gmail.com>
2024-12-10 14:47:36 +0800

    add json

--- llava/train/train.py
+++ llava/train/train.py
@@ -39,9 +39,7 @@ from PIL import Image
 import random
 from llava.visual_prompt_organizer import vip_processor, visual_prompt_config
 
-
 local_rank = None
-
 
 def rank0_print(*args):
     if local_rank == 0:
